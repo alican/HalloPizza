8aa4c6b671f11ee5f3b580a1e2e74d25032ce470
Alican Toprak <tordox@gmail.com>
2015-11-16 22:14:50 +0100

    baecker

--- baecker.php
+++ baecker.php
@@ -2,14 +2,11 @@
 <body>
 <section id="container">
     <?php include "includes/header.php" ?>
-
     <?php
-
     require_once('vendor/twig/twig/lib/Twig/Autoloader.php');
     Twig_Autoloader::register();
     $loader = new Twig_Loader_Filesystem('views');
-    $twig = new Twig_Environment($loader, array(
-        // 'cache' => 'cache',
+    $twig = new Twig_Environment($loader, array(// 'cache' => 'cache',
     ));
 
     include "includes/db/db_connect.php";
@@ -17,65 +14,94 @@
     $sql_fetch_orders = "SELECT * FROM orders ORDER BY -ordered LIMIT 10;";
 
     $orders = [];
+    $message = "";
 
-    if (isset($_GET["order"])){
+
+    // Hier wird geprüft ob es POST-Daten gibt.
+    // Wenn ja, dann wurde das Formular gesendet.
+    if($_POST){
+
+        $items = array();
+
+        // Hier gehen wir die POST-Daten durch und holen
+        // alle Daten die mit item- anfangen und stecken sie in ein array
+        foreach ($_POST as $key => $value) {
+            if (strstr(htmlspecialchars($key), 'item')) {
+                $k = str_replace('item-', '', $key);
+                $items[$k] = htmlspecialchars($value);
+            }
+        }
+
+        $sql_update_orders = "";
+
+        foreach($items as $k => $v) {
+            $sql_update_orders .= sprintf("UPDATE `order_items` SET `state`= %s WHERE `orderitemid`= %s;", $v, $k);
+        }
+
+        //echo $sql_update_orders;
+
+        if ($db->multi_query($sql_update_orders) === TRUE) {
+            $message = "Status wurde aktualisiert.";
+        } else {
+            $message = "Error updating record: " . $db->error;
+        }
+        $db->close();
+        include "includes/db/db_connect.php";
+    }
+
+    if (isset($_GET["order"])) {
         $orderID = htmlspecialchars($_GET["order"]);
 
         $fetch_order = sprintf("
-            SELECT i.name,
-                   i.picture,
-                   i.price,
-                   i.ingredients,
-                   i.itemid,
-                   oi.quantity,
-                   oi.orderitemid,
-                   oi.state
-            FROM order_items oi
-            JOIN items i ON oi.item = i.itemid
-            WHERE oi.order = %s;",
+                SELECT i.name,
+                       i.picture,
+                       i.price,
+                       i.ingredients,
+                       i.itemid,
+                       oi.quantity,
+                       oi.orderitemid,
+                       oi.state
+                FROM order_items oi
+                JOIN items i ON oi.item = i.itemid
+                WHERE oi.order = %s;",
             $orderID
         );
 
-        if(!$result = $db->query($fetch_order)){
+        if (!$result = $db->query($fetch_order)) {
             die('There was an error running the query [' . $db->error . ']');
         }
 
-        while($row = $result->fetch_assoc()){
+        while ($row = $result->fetch_assoc()) {
             $orders[] = $row;
         }
+    }else{
+        $message = "Bestellung auswählen um es zu bearbeiten.";
+
     }
-
-
-
     ?>
-
     <div id="content">
         <div id="left">
-            <form action="baecker.php" method="post">
-
-            <?php
-            echo $twig->render('baecker-order-view.html', array('queryResult'=> $orders));
-            ?>
-                <input type="submit" name="speichern" value="speichern" />
+            <form method="post">
+                <?php
+                    echo $twig->render('baecker-order-view.html',
+                        array('queryResult' => $orders, 'message' => $message));
+                ?>
             </form>
-
-
-
         </div>
         <div id="right">
 
             <section id="auftrag">
                 <h1 id="header_right">Aufträge</h1>
                 <?php
-                if(!$result = $db->query($sql_fetch_orders)){
-                die('There was an error running the query [' . $db->error . ']');
+                if (!$result = $db->query($sql_fetch_orders)) {
+                    die('There was an error running the query [' . $db->error . ']');
                 }
 
-                while($row = $result->fetch_assoc()){
+                while ($row = $result->fetch_assoc()) {
 
-                $results[] = $row;
+                    $results[] = $row;
                 }
-                echo $twig->render('order-list.html', array('queryResult'=> $results));
+                echo $twig->render('order-list.html', array('queryResult' => $results));
                 ?>
 
         </div>
